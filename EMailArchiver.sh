5a19330337821b2cf86eb064ff9337bfaff1d6d4
Christian <christian@edv-ehm.de>
2023-05-14 15:23:28 +0200

    pause.jar entfernt

--- EMailArchiver.sh
+++ EMailArchiver.sh
@@ -1,7 +1,6 @@
 #!/bin/bash
 #
-SCRIPTPATH=/home/christian/git/EMailArchiver
-#SCRIPTPATH=$(cd `dirname $0` && pwd)
+SCRIPTPATH=$(cd `dirname $0` && pwd)
 cd ${SCRIPTPATH}
 echo $SCRIPTPATH
 for filename in ${SCRIPTPATH}/lib/*.jar
@@ -10,5 +9,5 @@ done
 echo $CLASSPATH
 export CLASSPATH
 java -Xmx1024M -Xms512M org.myoggradio.Main
-java pack.Pause
+read dummy
 exit
