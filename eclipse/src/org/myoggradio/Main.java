ed0a9f7fef77038ac1eb003492b990a467b46e1b
Christian <christian@edv-ehm.de>
2023-05-19 18:22:40 +0200

    Versionsinfo bei Start

--- eclipse/src/org/myoggradio/Main.java
+++ eclipse/src/org/myoggradio/Main.java
@@ -3,6 +3,7 @@ package org.myoggradio;
 public class Main {
 
 	public static void main(String[] args) throws Exception {
+		System.out.println(Parameter.version);
 		Initialisieren ini = new Initialisieren();
 		ini.start();
 		MainMenu mm = new MainMenu();
